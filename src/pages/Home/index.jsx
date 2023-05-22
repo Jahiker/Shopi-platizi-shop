abce16c823f1c09b3d7359f5ee5d3ee57c3f797c
jahiker <rojasjahiker@gmail.com>
2023-05-22 00:23:15 -0400

    [add] api connection

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,26 @@
+import { useState, useEffect } from 'react'
+import axios from '../../axios'
+
 import Card from '../../components/Card'
+
 const Home = () => {
+  const [products, setProducts] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get('/products')
+      .then((response) => setProducts(response.data))
+      .catch((err) => console.error(err))
+  }, [])
+
   return (
     <div>
       <h1>Home</h1>
-      <Card />
+      <div className='grid w-full grid-cols-4 gap-4 p-8'>
+        {products?.map((product, index) => (
+          <Card key={`${product.id}-${index}`} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
